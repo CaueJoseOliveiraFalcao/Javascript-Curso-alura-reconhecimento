d0194020fd81cfeceda258f6c4941a21d75e936d
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2022-12-26 15:53:00 -0500

    gg

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -1,1 +1,11 @@
-console.log(Math.floor(Math.random() * 1000))
+'use strict'
+const MaiorValor = 100;
+const MenorValor = 1;
+document.querySelector('#menor-valor').innerHTML = MenorValor
+document.querySelector('#maior-valor').innerHTML = MaiorValor
+const NumeroAleatorio = gerarNumAleatorio();
+
+function gerarNumAleatorio(){
+    return Math.floor(Math.random() * MaiorValor+1)
+};
+
